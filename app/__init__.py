f97bba110081568c834e9f30207f5c2197204245
wish007 <326450058@qq.com>
2017-02-06 21:55:29 +0800

    add DB migration scripts

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,13 @@
 from flask import Flask
 # 将Flask-Bootstrap从flask.ext命名空间导入
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+db = SQLAlchemy(app)
+
 app.config.from_object('config')
 
-from app import views
+from app import views, models
--- config.py
+++ config.py
@@ -1,4 +1,10 @@
-
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 CSRF_ENABLED = True
 SECRET_KEY = 'ThisIsAHardGuessString.&uY^%FG0-@Fv'
+
+# 数据库文件的路径
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+# SQLAlchemy-migrate 数据文件存储的路径
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
